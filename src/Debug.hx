ca63e0b23a14b9ae0bf752dcd7c1012806e23882
Jay the Creator <jaanov2013@gmail.com>
2022-10-05 18:46:29 -0400

    fixed audio bug for stable haxe

--- src/Debug.hx
+++ src/Debug.hx
@@ -1,5 +1,9 @@
 class Debug {
 
+    //! I DO NOT, NOR WILL EVER WORK FOR YOUR WICKED ENGINE, SOURCE OR HSCRIPT MODS IF YOU MAKE THEM
+    //! IF YOU HAVE DEBUG ISSUES, THAT IS THE RESULT OF A MOD, PLEASE DO NOT DM ME, DM THE MOD CREATOR
+    //! PLEASE AND THANK YOU! - J
+
     public static var date = DateTools.format(Date.now(), "%Y-%m-%d__%H-%M-%S"); //used for file
     static var dateTime = DateTools.format(Date.now(), "%H:%M:%S"); //used for time of error
 
@@ -11,20 +15,29 @@ class Debug {
 
     public static var dfp = '$path\\$dfile';
     public static var debugFile:sys.io.FileOutput;
+
+    static var prefix = "[Wicked Engine]";
     
     public static function debug(v:Dynamic) {
-        var line = '[Wicked Engine] -=- ${v}';
+        var line = '$prefix -=- ${v}';
 
         trace(line); //logs on terminal,
         debugFile.writeString("\n"+line); //then logs in file for verifying later
     }
 
     public static function warn(v:Dynamic) {
-        var line = '[Wicked Engine (WARN)] !=! ${v}';
+        var line = '$prefix !=! ${v}';
 
         trace(line);
         debugFile.writeString('\n $line, TIME OF WARN: $dateTime'); //! if something doesn't seem right, use warn instead of debug!
     }
+
+    var message:Array<String> = [ //! PLEASE CHANGE THIS WHEN WORKING WITH SOURCE CODE!!
+        'D: time is $dateTime',
+        "D: {J:} if you run into any glitchiness, errors, or overall freakiness with Wicked Engine,",
+        "D: that is NOT part of the game (such as modcharts, or songs, or whatever,) please DM me, jay.hx#4380",
+        "D: {/J:} so that i can push out an update potentially fixing your issue ASAP!"
+    ];
 
     public function new(?file:String) {
         if (!fs.isDirectory(path) && !fs.exists(path) && file == null) { //if path isn't a directory, or doesn't exist, and we haven't been provided a file,
@@ -32,26 +45,23 @@ class Debug {
 
             debugFile = f.append('$dfp', false); //make the file at that path,
 
-            debugFile.writeString("[Wicked Engine] -=- D: successful file creation"); //first debug!
-            debug('D: first run, time is $dateTime'); //and log when we made it
-            debug('D: {J:} if you run into any glitchiness, errors, or overall freakiness with Wicked Engine,'); //* PLEASE CHANGE THIS IF YOU'RE EDITING THE SOURE CODE,
-            debug('D: that is NOT part of the game (aka, rebooting, etc), please DM jay.hx#4380'); //* I CANNOT HELP YOUR MOD BE PROGRAMMED PROPERLY, I JUST GIVE RESOURCES
-            debug('D: {/J:} so that i can push out an update potentially fixing your issue ASAP!');
-
+            debugFile.writeString('$prefix -=- D: successful file creation'); //first debug!
+            for (i in 0...message.length) {
+                debug(message[i]);
+            }
         } else { //or,
 
 
             if (file == null) { //we haven't been provided a file to edit to,
                 debugFile = f.append('$dfp', false); //make one
-                debugFile.writeString("[Wicked Engine] -=- D: successful file creation");
-                debug('D: {J:} if you run into any glitchiness, errors, or overall freakiness with Wicked Engine,');
-                debug('D: that is NOT part of the game (aka, rebooting, etc), please DM jay.hx#4380');
-                debug('D: {/J:} so that i can push out an update potentially fixing your issue ASAP!');
+                debugFile.writeString('$prefix -=- D: successful file creation');
+                for (i in 0...message.length) {
+                    debug(message[i]);
+                }
             } else { //or we HAVE,
-                debugFile = f.append('$file', false); //! append to that file, ! IF YOU GET ERRORS HERE, YOU FUCKED UP THE PATH!
-                debugFile.writeString('\n[Wicked Engine (WARN)] !=! D: hooked again elsewhere -- ERRORS WILL HAPPEN IN A DIFFERENT FILE, CHECK PREFIX');
+                debugFile = f.append('$file', false); //! append to that file, IF YOU GET ERRORS HERE, YOU FUCKED UP THE PATH!
+                debugFile.writeString('\n$prefix !=! D: hooked again elsewhere -- ERRORS WILL HAPPEN IN A DIFFERENT FILE, CHECK PREFIX');
             }
-
         }
     }
 
--- src/Main.hx
+++ src/Main.hx
@@ -2,10 +2,17 @@ import hxd.res.Sound;
 
 class Main extends hxd.App {
 
-    static var music:hxd.snd.Channel;
-    static var actualMusic:hxd.res.Sound;
+    var music:hxd.snd.Channel;
+    var actualMusic:hxd.res.Sound;
 
     override function init() {
+        @:privateAccess haxe.MainLoop.add(() -> {});
+        hxd.Window.getInstance().addEventTarget(onEvent);
+        //! THIS IS A BUG WITH STABLE BRANCH OF HAXE, THIS IS REQUIRED IF YOU'RE MODIFYING SOURCE CODE
+        //! OTHERWISE, SONGS, AUDIO, ANYTHING ELSE SYNCING TO RESOURCES *WILL* FAIL
+        //* thank you to Yanrishatum for pinning this solution to the #heaps channel in Haxe's discord server
+        //* https://discord.gg/ZQykrvyzjw <= invite link to the Haxe discord server, if you have a bug with Heaps, hit them up
+        //* i STRONGLY advise that you try everything else BEFORE going to them, be a smart programmer.
 
         hxd.Res.initEmbed();
 
@@ -17,8 +24,7 @@ class Main extends hxd.App {
             actualMusic = hxd.Res.wickedMenu_mp3; //! HEAPS DOESN'T LIKE -, USE ONE FULL WORD FOR IT
         }
 
-        music = actualMusic.play(true); //! ON MY BUILD, FOR SOME REASON, MUSIC PLAYS FOR NOT EVEN A SECOND, THEN CUTS OUT.
-        //TODO: figure out what the hell is happening here
+        music = actualMusic.play(true);
 
     }
 
@@ -29,6 +35,16 @@ class Main extends hxd.App {
     override function update(dt:Float) {
     }
 
+}
+
+function onEvent(event : hxd.Event) {
+    switch(event.kind) {
+        case EKeyDown: switch(event.keyCode) {
+            case 81: Debug.debug('M: closing!'); Sys.exit(0);
+            case _:
+        }
+        case _:
+    }
 }
 
 /*
