398fd3c9459e496dd1404312d15fc1d460281e3d
Jay the Creator <jaanov2013@gmail.com>
2022-10-05 16:59:38 -0400

    run a few compile tests; still inconclusive

--- src/Main.hx
+++ src/Main.hx
@@ -2,29 +2,31 @@ import hxd.res.Sound;
 
 class Main extends hxd.App {
 
+    static var music:hxd.snd.Channel;
+    static var actualMusic:hxd.res.Sound;
+
     override function init() {
 
         hxd.Res.initEmbed();
-
-        var music:Sound = null;
 
         new Debug();
 
         Debug.debug('M: Debug hook successful'); //* verify we're working!
 
-        if(hxd.res.Sound.supportedFormat(OggVorbis)){
-            music = hxd.Res.wickedMenu; //! HEAPS DOESN'T LIKE -, USE ONE FULL WORD FOR IT
+        if(hxd.res.Sound.supportedFormat(Mp3)){
+            actualMusic = hxd.Res.wickedMenu_mp3; //! HEAPS DOESN'T LIKE -, USE ONE FULL WORD FOR IT
         }
 
-        if (music != null) {
-            music.play(); //! ON MY BUILD, FOR SOME REASON, MUSIC PLAYS FOR ~3 SECONDS, THEN CUTS OUT.
-            //TODO: figure out what the hell is happening here
-        }
+        music = actualMusic.play(true); //! ON MY BUILD, FOR SOME REASON, MUSIC PLAYS FOR NOT EVEN A SECOND, THEN CUTS OUT.
+        //TODO: figure out what the hell is happening here
 
     }
 
     public static function main() {
         new Main();
+    }
+
+    override function update(dt:Float) {
     }
 
 }
